bd18518be4ea21962b1650e4dd26cef7bfbc82ce
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-20 16:00:23 +0100

    first

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+function onPlay (data) {
+    const time = data.seconds;
+console.log(time);
+    }
+    player.on('timeupdate', onPlay);
+
+
+
 
