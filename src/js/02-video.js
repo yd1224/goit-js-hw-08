a3509850404668f308706cfd9c45d2eb149f58b7
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-21 16:56:25 +0100

    finish vimeo-player-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,23 @@
 import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-function currentTime(){
-    player.on('timeupdate', function () {
-        let value = 0;
-        value += event.data.data.seconds;
-console.log(value);
+const videoTime = "videoplayer-current-time";
+function currentTime() {
+    const throttleFunc = throttle((event) => {
+        localStorage.setItem(videoTime, JSON.stringify(event.seconds))
+    }, 1000);
+
+    player.on('timeupdate', (event) => {
+        throttleFunc(event);
     });
-    }
+}
+
 currentTime();
+const timeUpdate = JSON.parse(localStorage.getItem(videoTime));
+player.setCurrentTime(timeUpdate);
+
 
 
 
