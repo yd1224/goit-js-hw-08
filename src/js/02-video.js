2bc2eb8b068b751a0bb963ba4a2374f66d2c4a07
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-22 18:26:49 +0100

    task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,4 @@
+
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+// const form = document.querySelector(".feedback");
 
+// form.addEventListener("input", handleInput);
+
+// function handleInput(event) {
+//     const emailValue = form.email.value;
+//     const messageValue = form.message.value;
+
+//     const feedbackFormState = {
+//         email: emailValue,
+//         message: messageValue
+//     };
+// }
+
+// form.addEventListener("submit", handleSubmit);
+// function handleSubmit(event) {
+//     event.preventDefault;
+//         const emailValue = form.email.value;
+//     const messageValue = form.message.value;
+
+//     const feedbackFormState = {
+//         email: emailValue,
+//         message: messageValue
+//     };
+// console.log(feedbackFormState);
+// }
+console.log("ertyui");
